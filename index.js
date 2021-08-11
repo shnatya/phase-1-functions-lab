7837ef85fa72dde30a0a175ba232436d12e17a65
Anastasia Shatokhina <shnatya@gmail.com>
2021-08-11 10:16:28 -0700

    commit tree 5374df540189b3405944f84e3a774520d5a97c60

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+    return Math.abs(blocks - 42);
+}
+
+function distanceFromHqInFeet(blocks) {
+    //console.log(distanceFromHqInBlocks(blocks) * 264);
+    return (distanceFromHqInBlocks(blocks) * 264);
+}
+
+function distanceTravelledInFeet(blocks1, blocks2) {
+    return Math.abs((blocks1 - blocks2) * 264);
+}
+
+function calculatesFarePrice(blocks1, blocks2) {
+    if ((distanceTravelledInFeet(blocks1, blocks2) < 400) || (distanceTravelledInFeet(blocks1, blocks2) === 400)) {
+        return 0;
+    }
+
+    else if (distanceTravelledInFeet(blocks1, blocks2) > 400 && distanceTravelledInFeet(blocks1, blocks2) < 2000) {
+        return ((distanceTravelledInFeet(blocks1, blocks2) - 400) * 2)/100;
+    }
+    
+    else if (distanceTravelledInFeet(blocks1, blocks2) > 2000 && distanceTravelledInFeet(blocks1, blocks2) < 2500) {
+        return 25;
+    }
+
+    else if (distanceTravelledInFeet(blocks1, blocks2) > 2500 || distanceTravelledInFeet(blocks1, blocks2) === 2500){
+        return ("cannot travel that far");
+    }
+}
